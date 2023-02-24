00e591838ff60892c865555df13a2ea3280aa537
_245_pawel <lanke@wp.pl>
2023-02-24 18:03:50 +0100

    komituję całość rozwiązania bez podziału na pakiety w związku z brakiem wyraźnego polecenia aby to zrobić

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+class Test {
+    public static void main(String[] args) {
+        Televisor televisor = new Televisor(false);
+        televisor.showStatus();
+        televisor.turnOn();
+        televisor.showStatus();
+        televisor.turnOff();
+        televisor.showStatus();
+    }
+}
+
